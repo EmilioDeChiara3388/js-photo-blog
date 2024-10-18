0e35aa1e4a2b91fcc56eb02737b94f498399018a
EmilioDeChiara3388 <emilio.dechiara3388@gmail.com>
2024-10-18 17:31:32 +0200

    fix: layout

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,6 @@
 //console.log("Funzioni?");
 
-let postEl = document.querySelector(".col-3");
+let postEl = document.querySelector(".row");
 
 axios.get("https://jsonplaceholder.typicode.com/photos?_limit=6").then(response => {
     let posts = response.data;
@@ -9,13 +9,15 @@ axios.get("https://jsonplaceholder.typicode.com/photos?_limit=6").then(response 
     posts.forEach(post => {
         let { title, thumbnailUrl } = post;
         console.log(post);
+        
         postEl.innerHTML += `
-            <div class="card debug">
+        
+            <div class="col-3 debug">
                 <img src="./assets/img/pin.svg" alt="" class="cardPin">
                 <img src=${thumbnailUrl} alt="" class="cardImage">
                 <p class="cardText">${title}</p>
             </div>
+            
         `
     })
 }).catch(err => console.error(err));
-
