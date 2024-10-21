cdcc635871fb998b98bad097f33d74d67d571768
EmilioDeChiara3388 <emilio.dechiara3388@gmail.com>
2024-10-21 16:25:33 +0200

    add: title with capitalized letters

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,42 +4,40 @@ let postEl = document.querySelector(".row");
 let overlayEl = document.querySelector(".overlay");
 let buttonEl = document.querySelector("button");
 let overlayImageEl = document.querySelector(".zoomedPic");
-//let overlayPicEl = document.querySelector(".overlayPic");
-
-
-
-
+let overlayPicEl = document.querySelector(".overlayPic");
 
 axios.get("https://jsonplaceholder.typicode.com/photos?_limit=6").then(response => {
     let posts = response.data;
-    console.log(posts);
-    
-    
+    //console.log(posts);
 
     posts.forEach(post => {
         let { title, thumbnailUrl } = post;
         //console.log(post);
-        console.log(thumbnailUrl);
-        
-        
+        //console.log(thumbnailUrl);
+        //console.log(title);
+
+        const capitalizedTitle = title.split();
+        for (let i = 0; i < capitalizedTitle.length; i++) {
+            capitalizedTitle[i] = capitalizedTitle[i][0].toUpperCase() + capitalizedTitle[i].substr(1);
+        }
+        //console.log(capitalizedTitle);
+        let rightTitle = capitalizedTitle.join(" ");
+
         postEl.innerHTML += `
             <div class="col-3 debug">
                 <img src="./assets/img/pin.svg" alt="" class="cardPin">
                 <img src=${thumbnailUrl} alt="" class="cardImage">
-                <p class="cardText">${title}</p>
+                <p class="cardText">${rightTitle}</p>
             </div>
         `
         let cardImageEl = document.querySelectorAll(".cardImage");
         //console.log(cardImageEl);
-        let overlayPicEl = document.querySelector(".overlayPic");
         for (let i = 0; i < cardImageEl.length; i++) {
             cardImageEl[i].addEventListener("click", () => {
                 overlayEl.classList.remove("hiddenOverlay");
-
                 posts.forEach(element => {
                     overlayImageEl.src = posts[i].thumbnailUrl;
                 })
-                
             })
         }
         buttonEl.addEventListener("click", () => {
