d06f718feee44fabf43ea6fe8b3b8bbbf173ca01
EmilioDeChiara3388 <emilio.dechiara3388@gmail.com>
2024-10-21 16:44:30 +0200

    add: comment

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,21 +1,25 @@
 //console.log("Funzioni?");
 
+//Seleziono elementi dall' HTML:
 let postEl = document.querySelector(".row");
 let overlayEl = document.querySelector(".overlay");
 let buttonEl = document.querySelector("button");
 let overlayImageEl = document.querySelector(".zoomedPic");
-let overlayPicEl = document.querySelector(".overlayPic");
 
+
+//Ottengo gli oggetti da aggiungere in pagina:
 axios.get("https://jsonplaceholder.typicode.com/photos?_limit=6").then(response => {
     let posts = response.data;
     //console.log(posts);
 
+    //Destrutturo oggetti ottenuti prendendo le keys da stampare in pagina:
     posts.forEach(post => {
         let { title, thumbnailUrl } = post;
         //console.log(post);
         //console.log(thumbnailUrl);
         //console.log(title);
 
+        //Rendo maiuscole le iniziali di ogni parola del titolo:
         const capitalizedTitle = title.split();
         for (let i = 0; i < capitalizedTitle.length; i++) {
             capitalizedTitle[i] = capitalizedTitle[i][0].toUpperCase() + capitalizedTitle[i].substr(1);
@@ -23,6 +27,7 @@ axios.get("https://jsonplaceholder.typicode.com/photos?_limit=6").then(response 
         //console.log(capitalizedTitle);
         let rightTitle = capitalizedTitle.join(" ");
 
+        //Aggiungo dinamicamente gli oggetti in pagina:
         postEl.innerHTML += `
             <div class="col-3 debug">
                 <img src="./assets/img/pin.svg" alt="" class="cardPin">
@@ -30,6 +35,8 @@ axios.get("https://jsonplaceholder.typicode.com/photos?_limit=6").then(response 
                 <p class="cardText">${rightTitle}</p>
             </div>
         `
+
+        //Aggiungo l'event listener su ogni immagine in pagina, per far apparire l'overlay con l'immagine cliccata zoommata:
         let cardImageEl = document.querySelectorAll(".cardImage");
         //console.log(cardImageEl);
         for (let i = 0; i < cardImageEl.length; i++) {
@@ -40,6 +47,8 @@ axios.get("https://jsonplaceholder.typicode.com/photos?_limit=6").then(response 
                 })
             })
         }
+
+        //Aggiungo l'event listener sul bottone per chiudere l'overlay:
         buttonEl.addEventListener("click", () => {
             overlayEl.classList.add("hiddenOverlay");
         })
