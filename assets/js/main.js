3b026622cecd45b70dafc1ac16e9838241f9ad12
EmilioDeChiara3388 <emilio.dechiara3388@gmail.com>
2024-10-21 14:23:56 +0200

    add: dinamic immage on overlay

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,16 +3,23 @@
 let postEl = document.querySelector(".row");
 let overlayEl = document.querySelector(".overlay");
 let buttonEl = document.querySelector("button");
+//let overlayPicEl = document.querySelector(".overlayPic");
+
+
 
 
 
 axios.get("https://jsonplaceholder.typicode.com/photos?_limit=6").then(response => {
     let posts = response.data;
     console.log(posts);
+    
+    
 
     posts.forEach(post => {
         let { title, thumbnailUrl } = post;
-        console.log(post);
+        //console.log(post);
+        console.log(thumbnailUrl);
+        
         
         postEl.innerHTML += `
             <div class="col-3 debug">
@@ -22,13 +29,22 @@ axios.get("https://jsonplaceholder.typicode.com/photos?_limit=6").then(response 
             </div>
         `
         let cardImageEl = document.querySelectorAll(".cardImage");
-        console.log(cardImageEl);
-            
+        //console.log(cardImageEl);
+        let overlayPicEl = document.querySelector(".overlayPic");
         for (let i = 0; i < cardImageEl.length; i++) {
             cardImageEl[i].addEventListener("click", () => {
                 overlayEl.classList.remove("hiddenOverlay");
-            })
-        }
+                
+                
+                
+                cardImageEl.forEach(image => {
+                overlayPicEl.innerHTML = `<img src=${thumbnailUrl} alt="" class="zoomedPic">`
+                })
+                    
+                })
+                
+            }
+        
         
         /* overlayEl.addEventListener("click", () => {
             overlayEl.classList.add("hiddenOverlay");
